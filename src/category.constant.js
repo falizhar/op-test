7f4434ae517c6a83d2ebefa13f3f3276946b76eb
Falah Sultan Alizhar <hi@alizhar.dev>
2022-09-23 11:52:53 +0700

    initial commit

---
+++ src/category.constant.js
@@ -0,0 +1,14 @@
+export const reliefCategory = {
+  'TK0': 54000000,
+  'K0': 58500000,
+  'K1': 63000000,
+  'K2': 67500000,
+  'K3': 72000000,
+}
+
+export const taxCategory = {
+  levelOne: 0,
+  levelTwo: 50000000,
+  levelThree: 250000000,
+  levelFour: 500000000,
+}
---
+++ src/pajak.test.js
@@ -0,0 +1,13 @@
+import { calculateTaxRelief, calculateTaxScheme } from './pajak.js';
+/**
+ * Dummy test
+ */
+test('person data has the right type to be calculated', () => {
+  it("works if true is truthy", () => {
+    expect(calculateTaxRelief).toBeTruthy()
+  })
+
+  it("DummyClass is instantiable", () => {
+    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  })
+})
